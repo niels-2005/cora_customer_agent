4dd35db56521ba57badde795c79a9ff65c012bf7
niels-2005 <niels.scholz01@web.de>
2025-11-06 16:33:39 +0100

    Refactor MCP integration and enhance product information retrieval in notebook and MCP server

--- src/cora_customer_agent/mcp.py
+++ src/cora_customer_agent/mcp.py
@@ -1,27 +1,52 @@
 from mcp.server.fastmcp import FastMCP
-from .utils.load_vector_store import load_vector_store
+from cora_customer_agent.utils.load_vector_store import load_vector_store
 
 mcp = FastMCP("Customer Chatbot MCP Server")
 
-vector_store = load_vector_store(collection_name="company_faq", init_vector_store=False)
+vector_store_faq = load_vector_store(
+    collection_name="company_faq", init_vector_store=False
+)
+vector_store_products = load_vector_store(
+    collection_name="company_products", init_vector_store=False
+)
 
 
 @mcp.tool()
 async def get_company_faq_answers(query: str) -> str:
     """
-    Use this tool to answer customer questions based on the company's FAQ documents.
+    Answers customer questions based on the company's FAQ documents.
+
+    Use this tool for general inquiries about company policies, products, or services.
+    It performs a similarity search and returns the most relevant FAQ answer.
 
     Args:
-        query (str): The customer's question.
+        query (str): The customer's question, e.g., 'How do I reset my password?'.
+
+    Returns:
+        str: The content of the most relevant FAQ answer.
     """
-    results = await vector_store.asimilarity_search(query, k=1)
+    results = await vector_store_faq.asimilarity_search(query, k=1)
 
     return results[0].page_content
 
 
 @mcp.tool()
-def get_product_informations():
-    pass
+async def get_product_informations(query: str) -> str:
+    """
+    Provides product information to answer customer questions based on company product documents.
+
+    Use this tool for detailed inquiries about specific products, features, or specifications.
+    It performs a similarity search and returns the most relevant product information.
+
+    Args:
+        query (str): The customer's question, including product details, e.g., 'Tell me about the HiveSmart Light Bulb A60'.
+
+    Returns:
+        str: The content of the most relevant product information.
+    """
+    results = await vector_store_products.asimilarity_search(query, k=1)
+
+    return results[0].page_content
 
 
 if __name__ == "__main__":
